c6fcf97b3725715e850384d0e59d3edb6c2e971d
merlynstefany <merlynstefany123@gmail.com>
2020-03-03 01:50:12 +0700

    1:50 03-Mar-20

---
+++ app/src/main/java/com/merlyn/droidcafe/OrderActivity.java
@@ -0,0 +1,21 @@
+package com.merlyn.droidcafe;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+
+}
